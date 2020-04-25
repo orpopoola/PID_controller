e55f518680a2d15d76d958ecbb33e26618969159
orpopoola <olaoluwapopoola@gmail.com>
2020-04-25 23:29:26 +0100

    Editted twiddle code

--- src/main.cpp
+++ src/main.cpp
@@ -41,7 +41,26 @@ std::vector<double> dp = {0.05, 0.001, 0.05};
 void twiddle(PID &pid_control) {
   std::cout << "State: " << twiddle_state_ << std::endl;
   std::cout << "PID Error: " << pid_control.TotalError() << ", Best Error: " << twiddle_best_error_ << std::endl;
-  
+  if (twiddle_state_ == 0) {
+    twiddle_best_error_ = pid_control.TotalError();
+    p[twiddle_idx] += dp[twiddle_idx];
+    twiddle_state_ = 1;
+  } 
+  } else { //twiddle_state_ = 2
+    if (pid_control.TotalError() < twiddle_best_error_) {
+      twiddle_best_error_ = pid_control.TotalError();
+      dp[twiddle_idx] *= 1.1;
+      twiddle_idx = (twiddle_idx + 1) % 3;
+      p[twiddle_idx] += dp[twiddle_idx];
+      twiddle_state_ = 1;
+    } else {
+      p[twiddle_idx] += dp[twiddle_idx];
+      dp[twiddle_idx] *= 0.9;
+      twiddle_idx = (twiddle_idx + 1) % 3;
+      p[twiddle_idx] += dp[twiddle_idx];
+      twiddle_state_ = 1;
+      //pid.Init(p[0], p[1], p[2]);
+    }
   }
   pid_control.Init(p[0], p[1], p[2]);
 }
